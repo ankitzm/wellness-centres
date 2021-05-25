34308db19d752598efda4c05c45af38578c19b5c
ankitzm <ankit.singh.zm@gmail.com>
2021-05-25 16:49:58 +0530

    Updates

---
+++ precache-manifest.493f0958b25c093d0979cb1b7343e864.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ada73f9482f1c3add29c84bb1787df19",
+    "url": "/wellness-records/index.html"
+  },
+  {
+    "revision": "8c1c07f23d89a8ba7fe2",
+    "url": "/wellness-records/static/css/main.3f776259.css"
+  },
+  {
+    "revision": "8c1c07f23d89a8ba7fe2",
+    "url": "/wellness-records/static/js/main.466379c9.js"
+  },
+  {
+    "revision": "95ea6e06508ae6cb347ab110e0d2cd19",
+    "url": "/wellness-records/static/js/main.466379c9.js.LICENSE.txt"
+  }
+]);
